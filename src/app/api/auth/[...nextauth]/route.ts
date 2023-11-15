8e9271c8964ab2d5803852c2ad0758c0037cae75
Igor Goncalo <igorlabakig@gmail.com>
2023-11-15 22:15:40 +0000

    s

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth/next";
+import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import { prismaClient } from '@/services/prismaClient';
@@ -89,7 +89,7 @@ export const authOptions = {
     },
   },
   pages: {
-    signIn: '/auth',
+    signIn: '/signin',
     error: '/',
   },
   session: {
