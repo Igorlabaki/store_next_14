3365215aa26767d834c459b9d3322948e85e9be1
Igor Goncalo <igorlabakig@gmail.com>
2023-11-15 22:52:15 +0000

    s

--- src/serverActions/actions/product/list.ts
+++ src/serverActions/actions/product/list.ts
@@ -6,7 +6,7 @@ export const productListServerAction = async (search: string | undefined) => {
   let queryArgs = {};
 
   if (search && search != "All") {
-    queryArgs = {
+    const listProductd = await prismaClient.product.findMany({
       where: {
         name: {
           contains: search,
@@ -16,20 +16,17 @@ export const productListServerAction = async (search: string | undefined) => {
         brand: true,
       },
       take: 6,
-    };
-    const listProductd = await prismaClient.product.findMany(queryArgs);
+    });
 
     return listProductd;
   }
 
-  queryArgs = {
+  const listProductd = await prismaClient.product.findMany({
     include: {
       brand: true,
     },
     take: 6
-  };
-
-  const listProductd = await prismaClient.product.findMany(queryArgs);
+  });
 
   return listProductd;
 };
