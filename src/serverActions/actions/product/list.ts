22b7bbf263a61b5a7ffee2d7da44cda56c9715cc
Igor Goncalo <igorlabakig@gmail.com>
2023-11-15 22:42:59 +0000

    r

--- src/serverActions/actions/product/list.ts
+++ src/serverActions/actions/product/list.ts
@@ -1,34 +1,35 @@
-"use server"
+"use server";
 
 import { prismaClient } from "@/services/prismaClient";
 
 export const productListServerAction = async (search: string | undefined) => {
-    let queryArgs = {}
-    const query = () => {
+  let queryArgs = {};
 
-        if(search && search != "All"){
-            queryArgs = {
-                where:{
-                    name: {
-                        contains: search
-                    }
-                },
-                include:{
-                    brand: true
-                }   
-            }
-            return queryArgs;
-        }
-        
-        queryArgs = {
-            include: {
-                brand: true
-            }
-        }
-        return queryArgs;
-    }
-    
-    const listProductd = await prismaClient.product.findMany()
+  if (search && search != "All") {
+    queryArgs = {
+      where: {
+        name: {
+          contains: search,
+        },
+      },
+      include: {
+        brand: true,
+      },
+      take: 6,
+    };
+    const listProductd = await prismaClient.product.findMany(queryArgs);
 
-    return listProductd
+    return listProductd;
+  }
+
+  queryArgs = {
+    include: {
+      brand: true,
+    },
+    take: 6
+  };
+
+  const listProductd = await prismaClient.product.findMany(queryArgs);
+
+  return listProductd;
 };
