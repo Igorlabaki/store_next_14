c14702393352598b4fdf4d4dd0f2cc12e2582dfd
Igor Goncalo <igorlabakig@gmail.com>
2023-11-16 13:49:22 +0000

    s

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -15,7 +15,7 @@ export default async function CartPage() {
       <h1 className="text-[1.125rem] tracking-[0.25rem] leading-[2.5rem] mt-[2.5rem] text-center">
         CHEKOUT
       </h1>
-      <div className="px-[1rem] mt-[2rem] flex flex-col justify-start items-start gap-y-[2rem] h-full w-full">
+      <div className="px-[1rem] mt-[2rem] flex flex-col justify-start items-start gap-y-[2rem] h-full w-full flex-1">
         <ListProductsCartComponent userCartProductList={useCart?.ProductCart} />
         <DividerComponent />
         <div className="flex w-full justify-between items-center">
