455538f36419b748c8b7eff3c5eacc01bafd4dda
Igor Goncalo <igorlabakig@gmail.com>
2023-11-16 10:46:41 +0000

    d

--- src/serverActions/actions/cart/deleteCart.ts
+++ src/serverActions/actions/cart/deleteCart.ts
@@ -1,11 +1,9 @@
-"use server"
+"use server";
+
+import deleteCart from "@/serverActions/prismaRepository/cart/deleteCart";
 import { prismaClient } from "@/services/prismaClient";
 
 export default async function deleteCartServerAction(userId: string) {
-    const deleteCart = await prismaClient.cart.delete({
-        where:{
-            userId,
-        }
-    })
-    return deleteCart;
+  const deletedCart = await deleteCart(userId);
+  return deletedCart;
 }
