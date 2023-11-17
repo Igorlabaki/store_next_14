86eb87765b6d8e83346cd417983f93c06dd00466
Igor Goncalo <igorlabakig@gmail.com>
2023-11-17 17:04:14 +0000

    updat product list

--- src/serverActions/product/list.ts
+++ src/serverActions/product/list.ts
@@ -1,11 +1,13 @@
 "use server";
 
 import { listProductFactory } from "@/backend/useCase/product/listCase/listFactory";
-import { prismaClient } from "@/services/prismaClient";
+import { revalidatePath } from "next/cache";
 
 export const productListServerAction = async (search: string | undefined) => {
   
   const listProduct = await listProductFactory().handle(search)
+
+  revalidatePath("/product/list")
 
   return listProduct;
 };
