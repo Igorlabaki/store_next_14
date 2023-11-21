1313e9125586342f642863b85ffacd965886d393
Igor Goncalo <igorlabakig@gmail.com>
2023-11-21 13:35:01 +0000

    loadin product list page

--- src/app/product/list/loading.tsx
+++ src/app/product/list/loading.tsx
@@ -9,7 +9,7 @@ export default function loading() {
      <div className="flex flex-1 min-h-screen flex-col gap-y-[1rem] py-[2rem]">
         {itemsList.map((item : number) => {
             return(
-                <div className="flex h-[8.375rem] gap-x-[0.75rem] bg-gray-200 animate-pulse border-[2px] rounded-md shadow-lg overflow-hidden relative" key={item} />
+                <div className="flex h-[8.375rem] gap-x-[0.75rem] bg-gray-300 animate-pulse border-[2px] rounded-md shadow-lg overflow-hidden relative" key={item} />
             )
         })}
      </div>
